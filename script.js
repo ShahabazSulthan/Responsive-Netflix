105d8296ad99ce4fc67b8b3b87fffff4a4d16a4c
ShahabazSulthan <158268765+ShahabazSulthan@users.noreply.github.com>
2024-02-08 13:59:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$("#Questions1").click(function(){
+    $("#Answer1").toggle();
+     });
+  
+     $("#Questions2").click(function(){
+    $("#Answer2").toggle();
+     });
+  
+     $("#Questions3").click(function(){
+    $("#Answer3").toggle();
+     });
+  
+     $("#Questions4").click(function(){
+    $("#Answer4").toggle();
+     });
+  
+     $("#Questions5").click(function(){
+    $("#Answer5").toggle();
+     });
+  
+     $("#Questions6").click(function(){
+    $("#Answer6").toggle();
+     });
+
+     
